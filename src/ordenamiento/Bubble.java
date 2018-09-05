1e765e32251aa7d46ae7733788f76bedd3cafe50
David <davbetm@gmail.com>
2018-09-05 12:51:18 -0500

    Clase 6: Contando las operaciones de insert sort y bubble sort para el peor y mejor de los casos con una entrada de 17

--- src/ordenamiento/Bubble.java
+++ src/ordenamiento/Bubble.java
@@ -1,5 +1,7 @@
 package ordenamiento;
 
+import tools.GenerarDatos;
+
  /* @author david */
 public class Bubble implements SortAlgorithm {
     private long initTime;
@@ -16,10 +18,14 @@ public class Bubble implements SortAlgorithm {
     public void sort(double[] data) {
         this.initTime = System.currentTimeMillis();
         double temp;
+        int cont = 0;
         for (int i = 1; i < data.length; i++) {
+            cont += 3;
             for (int j = 0; j < data.length - 1; j++) {
+                cont += 8;
                 // Se compara
                 if(data[j] > data[j+1]) {
+                    cont += 9;
                     temp = data[j];
                     data[j] = data[j+1];
                     data[j + 1] = temp;
@@ -28,10 +34,17 @@ public class Bubble implements SortAlgorithm {
         }
         this.endTime = System.currentTimeMillis();
         this.totalTime = this.endTime - this.initTime;
+        System.out.println("Número de operaciones: " + cont);
     }
 
     @Override
     public long getTotalTime() {
         return totalTime;
+    }
+    
+    public static void main(String []args) {
+        Bubble b = new Bubble();
+        b.sort(GenerarDatos.generarDatosMejorCaso(17));
+        b.sort(GenerarDatos.generarDatosPeorCaso(17));
     }
 }
--- src/ordenamiento/InsertSort.java
+++ src/ordenamiento/InsertSort.java
@@ -1,4 +1,7 @@
 package ordenamiento;
+
+import tools.GenerarDatos;
+
 /**
  *
  * @author david
@@ -17,27 +20,48 @@ public class InsertSort implements SortAlgorithm{
     @Override
     public void sort(double[] data) {
         this.initTime = System.currentTimeMillis();
+        int n = data.length;
+        //Contando el número de operaciones
+        int cont = 0;
         double key;
         int j;
-        for (int i = 1; i < data.length; i++) {
+        for (int i = 1; i < n; i++) {
+            cont += 3; // por las 3 operaciones que implica el ciclo for.
             key = data[i];
+            cont += 2; // por las 2 operaciones que implica el acceso a memoria
+            // y asignación.
             j = i - 1;
+            cont += 2; // por las 2 operaciones, resta y asignación.
+            
             /* Se mueven hacia adelante los elementos de data[0,...i-1]
             hasta que la llave sea mayor o igual que data[j]
             */
             while(j >= 0 && data[j] > key) {
+                cont += 4; // por las 4 operaciones del ciclo while
                 data[j+1] = data[j];
+                cont += 4; // suma, acceso a memoria, acceso a memoria y asignación
                 j--;
+                cont++; // operación de incremento, se considera 1
             }
             // Insertamos la llave en donde corresponde
             data[j + 1] = key;
+            cont += 3; // suma, acceso a memoria y asignación
         }
         this.endTime = System.currentTimeMillis();
         this.totalTime = this.endTime - this.initTime;
+        // El muestra el número de operaciones contadas
+        System.out.println("Númeor de operaciones: " + cont);
     }
 
     @Override
     public long getTotalTime() {
         return totalTime;
+    }
+    
+    public static void main(String []args) {
+        InsertSort is = new InsertSort();
+        
+        is.sort(GenerarDatos.generarDatosMejorCaso(17));
+        is.sort(GenerarDatos.generarDatosPeorCaso(17));
     }
 }
